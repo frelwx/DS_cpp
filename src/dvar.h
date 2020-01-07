de9c4efa86e43232736d4213bbe61085aff62d6c
frelwx <1228145131@qq.com>
2020-01-07 08:52:48 +0800

    str

--- src/dvar.h
+++ src/dvar.h
@@ -516,7 +516,7 @@ dvar operator> (const dvar &I1, const dvar &I2)
 }
 dvar operator== (const dvar &I1, const dvar &I2)
 {
-    if(I1.gettype() != I2.gettype() && (I1.gettype() == is_string || I2.gettype() == is_string)) return dvar(false);
+    if(I1.gettype() != I2.gettype() && 1) return dvar(false);
     
     int max_convert = std::max(I1.gettype(), I2.gettype());
     dvar tmp;
